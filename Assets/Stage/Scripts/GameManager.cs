cf4c5cdd390dbf5e58946cc6ae8c95758de37d72
robottkang <robottkang39@gmail.com>
2022-10-13 08:35:39 +0900

    Style : 코드 가시성 높이게 정리, Feat : selectionBox 위치 정보 추가
    주석 추가, FieldBorad의 field -> borad로 명칭 변경
    selectionBox 위치 정보를 담을 변수 추가

--- Assets/Stage/Scripts/GameManager.cs
+++ Assets/Stage/Scripts/GameManager.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int inputValue = 0;
-    public int stage = 1;
+    public int inputChildCount = -1;
+    private int _stageNum = 0;
     public float timeLimit;
     [HideInInspector]
     public FieldBorad field;
+    private Vector2Int _selectionBoxPositionInBorad = new Vector2Int(2,2);
+
+    /// <summary>
+    /// stageNum show current stage number (Every time stageNum set bigger than 0 or differently from previous value, currentGameState become Play State).
+    /// </summary>
+    public int stageNum
+    {
+        get => _stageNum;
+        set
+        {
+            if (value < 0)
+            {
+                _stageNum = 0;
+                return;
+            }
+
+            if (_stageNum != value)
+            {
+                GameSetting.currentGameState = GameSetting.GameState.Play;
+                _stageNum = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Box can position in range ([1, rows - 1),[1, columns - 1)).
+    /// </summary>
+    public Vector2Int selectionBoxPositionInBorad
+    {
+        get => _selectionBoxPositionInBorad;
+        set => _selectionBoxPositionInBorad = new Vector2Int(Mathf.Clamp(value.x, 1, FieldBorad.rows - 2), Mathf.Clamp(value.y, 1, FieldBorad.columns - 2));
+    }
 
     private void Awake()
     {
@@ -17,18 +49,32 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (inputValue != 0)
+        switch (GameSetting.currentGameState)
         {
-            if (SelectBox(inputValue / 5, inputValue % 5) == FindMaxValue())
+            case GameSetting.GameState.NotPlay:
+                CompareValueWithMaxValue();
+                break;
+            case GameSetting.GameState.Play:
+                CompareValueWithMaxValue();
+                break;
+            case GameSetting.GameState.Pause:
+                break;
+            case GameSetting.GameState.Failure:
+                break;
+        }
+    }
+
+
+    private void CompareValueWithMaxValue()
+    {
+        if (inputChildCount != -1)
+        {
+            if (SelectBox(selectionBoxPositionInBorad.x, selectionBoxPositionInBorad.y) == FindMaxValue())
             {
-                stage++;
-                RearrangeBorad(stage);
+                stageNum++;
+                RearrangeBorad(stageNum / 4 + 3);
             }
-            else
-            {
-                FindMaxValue();
-            }
-            inputValue = 0;
+            inputChildCount = -1;
         }
     }
 
@@ -37,15 +83,15 @@ public class GameManager : MonoBehaviour
         int maxValue = 0;
         int valueSum = 0;
 
-        for (int boradGlobalX = 1; boradGlobalX < 4; boradGlobalX++)     // 3*3 range center value of x
+        for (int boradGlobalX = 1; boradGlobalX < 4; boradGlobalX++)     // x of 3*3 range center value
         {
-            for (int boradGlobalY = 1; boradGlobalY < 4; boradGlobalY++) // 3*3 range center value of y
+            for (int boradGlobalY = 1; boradGlobalY < 4; boradGlobalY++) // y of 3*3 range center value
             {
                 for (int boradLocalX = boradGlobalX - 1; boradLocalX <= boradGlobalX + 1; boradLocalX++)     // int range [boradGlobalX - 1, boradGlobalX + 1]
                 {
                     for (int boradLocalY = boradGlobalY - 1; boradLocalY <= boradGlobalY + 1; boradLocalY++) // int range [boradGlobalY - 1, boradGlobalY + 1]
                     {
-                        valueSum += field.field[boradLocalX, boradLocalY].GetComponent<GameFieldSpace>().value;
+                        valueSum += field.borad[boradLocalX, boradLocalY].GetComponent<GameFieldSpace>().value;
                     }
                 }
 
@@ -57,11 +103,14 @@ public class GameManager : MonoBehaviour
                 valueSum = 0;
             }
         }
-
         return maxValue;
     }
 
-
+    /// <summary>
+    /// return sum value of range 3*3.
+    /// </summary>
+    /// <param name="x">x of Box's center</param>
+    /// <param name="y">y of Box's center</param>
     public int SelectBox(int x, int y)
     {
         int valueSum = 0;
@@ -69,38 +118,47 @@ public class GameManager : MonoBehaviour
         {
             for (int j = y - 1; j <= y + 1; j++)
             {
-                valueSum += field.field[i, j].GetComponent<GameFieldSpace>().value;
+                valueSum += field.borad[i, j].GetComponent<GameFieldSpace>().value;
             }
         }
 
         Debug.Log(valueSum);
         return valueSum;
     }
-    public void RearrangeBorad(int stageNum)
+
+    /// <summary>
+    /// Rearrange value of borad at random.
+    /// </summary>
+    /// <param name="rangeMax">int range (1, rangeMax)</param>
+    public void RearrangeBorad(int rangeMax)
     {
         for (int x = 0; x < FieldBorad.columns; x++)
         {
             for (int y = 0; y < FieldBorad.rows; y++)
             {
-                field.field[x, y].GetComponent<GameFieldSpace>().value = Random.Range(1, (stageNum + 8) / 5 + 1);
+                field.borad[x, y].GetComponent<GameFieldSpace>().value = Random.Range(1, rangeMax);
             }
         }
     }
 
+    /// <summary>
+    /// initialize borad and rearrange borad value
+    /// </summary>
     public void InitializeBorad()
     {
         int spaceCount = 0;
-        for (int x = 0; x < FieldBorad.columns; x++)
+        for (int y = 0; y < FieldBorad.rows; y++)
         {
-            for (int y = 0; y < FieldBorad.rows; y++)
+            for (int x = 0; x < FieldBorad.columns; x++)
             {
-                field.field[x, y] = field.fieldObject.transform.GetChild(spaceCount).gameObject;
-                field.field[x, y].GetComponent<GameFieldSpace>().childCount = spaceCount;
+                field.borad[x, y] = field.fieldObject.transform.GetChild(spaceCount).gameObject;
+                field.borad[x, y].GetComponent<GameFieldSpace>().childCount = spaceCount;
                 spaceCount++;
             }
         }
-        RearrangeBorad(stage);
+        RearrangeBorad(2);
     }
+
 }
 
 [System.Serializable]
@@ -109,20 +167,20 @@ public class FieldBorad
     public GameObject fieldObject;
     public static int rows = 5;
     public static int columns = 5;
-    public GameObject[,] field = new GameObject[columns, rows];
+    public GameObject[,] borad = new GameObject[columns, rows];
 }
 
-public partial class GameSetting
+public static partial class GameSetting
 {
-    public GameState currentGameState = GameState.NotPlay;
-    private bool _whetherDidTutorial;
-    public bool whetherDidTutorial
+    public static GameState currentGameState = GameState.NotPlay;
+
+    public static bool whetherDidTutorial
     {
         get
         {
             if (PlayerPrefs.HasKey("Tutorial"))
             {
-                return PlayerPrefs.GetInt("Tutorial") > 0 ? true : false;
+                return PlayerPrefs.GetInt("Tutorial") > 0;
             }
             else
             {
