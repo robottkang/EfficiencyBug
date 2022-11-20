8e8fd80bfd24ab7e385028100436cdaf57b7dd92
robottkang <robottkang39@gmail.com>
2022-11-20 14:51:02 +0900

    Feat : 중앙 메뉴 스크립트 제작
    애니메이션을 관리하는 스크립트

---
+++ Assets/Stage/Menu/Middle Menu/Temp/TempButton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameManager gameManager;
+    [SerializeField]
+    private Animator animator;
+    private GameSetting.GameState beforeGameState = GameSetting.currentGameState;
+
+    private void Awake()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        if (gameManager == null)
+        {
+            gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        }
+    }
+
+    private void Update()
+    {
+        if (beforeGameState != GameSetting.currentGameState && GameSetting.currentGameState != GameSetting.GameState.Pause)
+        {
+            if (GameSetting.currentGameState == GameSetting.GameState.Play)
+            {
+                animator.SetTrigger("Disappear");
+            }
+            else if (GameSetting.currentGameState == GameSetting.GameState.NotPlay)
+            {
+                animator.SetTrigger("Appear");
+            }
+
+            beforeGameState = GameSetting.currentGameState;
+        }
+    }
+}
